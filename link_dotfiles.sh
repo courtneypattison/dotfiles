449d9aa5088e60014eb971520908676014bf2507
mlisccourtney <mliscourtney@gmail.com>
2016-01-01 15:04:19 -0500

    added a bash script for linking my dotfiles to my home directory

---
+++ link_dotfiles.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ln -sf $DIR/.vimrc ~
+ln -sf $DIR/.bash_profile ~
+ln -sf $DIR/.gitignore_global ~
+ln -sf $DIR/.gitconfig ~
+ln -sf $DIR/.inputrc ~
+
