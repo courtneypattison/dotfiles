a2c9d38d22aa6316f863127834b203eed2f202cf
courtneypattison <courtneyp@ttison.ca>
2021-02-23 13:57:55 -0500

    Removed unused dotfiles

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -2,9 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sf $DIR/.vimrc ~
-ln -sf $DIR/.bash_profile ~
-ln -sf $DIR/.gitignore_global ~
-ln -sf $DIR/.gitconfig ~
-ln -sf $DIR/.inputrc ~
 ln -sf $DIR/.zshrc ~
-git config --global core.excludesfile ~/.gitignore_global
+ln -sf $DIR/.gitconfig ~
